69ec8ec73c615a1e103b97ded0667477e574da10
Pmsclima <pmsclima@gmail.com>
2025-12-19 00:24:13 +0000

    bootstrap springboot project

---
+++ src/main/java/com/pedro/scheduledtransfers/ScheduledTransfersApiApplication.java
@@ -0,0 +1,12 @@
+package com.pedro.scheduledtransfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledTransfersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledTransfersApiApplication.class, args);
+	}
+}
